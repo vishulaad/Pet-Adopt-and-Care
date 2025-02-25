8d745b3d4dd72f35b8611d5f84f6c3eab1fec6bc
vishulaad <127011625+vishulaad@users.noreply.github.com>
2025-02-25 23:12:06 +0530

    Add files via upload

---
+++ BackEnd/src/main/java/com/app/Happypet1Application.java
@@ -0,0 +1,15 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Happypet1Application {
+
+	public static void main(String[] args) 
+	{		
+		SpringApplication.run(Happypet1Application.class, args);
+	}
+
+}
